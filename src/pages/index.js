c12f3ff6ecb9fbf5fbdab7e269e711759e297c96
asdromundo <sebastiandromundo@outlook.com>
2020-06-25 09:18:24 -0500

    Fresh start, less dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import BlogContainer from "../components/blog-container"
+import BlogPosts from "../components/blog-posts"
 
-const IndexPage = () => (
+const Home = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <BlogContainer>
+      <SEO title="Inicio - Bienvenid@s" />
+      <h1>Así que vamos a intentarlo una vez más</h1>
+      <p>Sean bienvenidos a asdrome.digital</p>
+      <p>El futuro... Es hoy</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img
+          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+          alt="Group of pandas eating bamboo"
+        />
+      </div>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      <BlogPosts />
+    </BlogContainer>
   </Layout>
 )
 
-export default IndexPage
+export default Home
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
