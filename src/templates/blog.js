05ceab921614cf182476b2b58491ca35cca68c92
Antonio S. Dromundo E <sabesdromesco@gmail.com>
2020-07-17 03:07:15 -0500

    Add files via upload

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import BlogContainer from "../components/blog-container"
-import SEO from "../components/seo"
 
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
@@ -10,15 +9,14 @@ export default function Template({ data }) {
   return (
     <Layout>
       <BlogContainer>
-        <SEO title={frontmatter.title} />
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </div>
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
       </BlogContainer>
     </Layout>
   )
@@ -32,6 +30,10 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
       }
     }
   }
